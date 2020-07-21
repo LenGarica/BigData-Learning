96058a1decc50f607b24d1732af9e97db10c13f4
zyx <1428431375@qq.com>
2020-07-21 14:48:34 +0800

    update sparksql

--- Spark/codes/SparkSQL/README.md
+++
@@ -1,7 +0,0 @@
-# Spark项目实战
-
-本目录中有一个Actual-Project和一个learning-project，前者是本阶段学习整体完成后，进行的实战项目，后者是阶段性学习时，日常的测试。
-
-Actual-Project里面是使用Spark分析慕课网日志，实现主站最受学生欢迎的课程。
-
-learning-project里面是平时学习spark-sql中的各种方法。RDD、DataFrame、DataSet的操作。
--- Spark/codes/SparkSQL/scala/sparksqlpreliminary/HiveContextApp.scala
+++ Spark/codes/SparkSQL/scala/sparksqlpreliminary/HiveContextApp.scala
@@ -10,7 +10,7 @@ object HiveContextApp {
     // 1. 设置sparkConf
     val sparkConf = new SparkConf()
     //本地测试的时候要使用这句话，如果提交到服务器时，这句话可以不写
-//    sparkConf.setAppName("HiveContextApp").setMaster("local[2]")
+    sparkConf.setAppName("HiveContextApp").setMaster("local[2]")
 
     // 2. 设置spark环境
     val sparkContext = new SparkContext(sparkConf)
